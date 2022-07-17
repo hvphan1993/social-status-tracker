4be2a57acce2015e39eafadb44d7376cd26f25ff
hvphan <hvphan1993@gmail.com>
2022-07-17 00:53:01 -0700

    updated server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/social-network-api', {
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
