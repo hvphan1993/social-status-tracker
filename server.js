5c8dbb1e827845fe715ce0c8735e6de006f9b92b
hvphan <hvphan1993@gmail.com>
2022-07-17 00:55:51 -0700

    corrected server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/social-network-api', {
-  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
